c45e0e7eceaef7d8fd7de2bccb4737aa0f743ef3
Mateusz Kaleta <mateusz.b.kaleta@gmail.com>
2023-11-27 01:44:03 +0100

    e

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,3 +1,9 @@
 def print_results_from_list(my_list):
     for element in my_list:
         print(element)
+
+
+def append_results_to_list(results):
+    results = []
+    for result in results:
+        results.append(result.text)
